ecc048407520ebdbfb1e96d9080b773945e33f01
NicolasMacedoo <nicolas.conceicao01@gmail.com>
2022-09-01 20:25:24 -0300

    Alterada a estrutura renderizada por App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,18 @@
-function App() {
-  return <h1>Hello world</h1>
+import React from "react";
+import './App.css';
+
+import SearchBar from '../SearchBar/SearchBar';
+import Film from '../Film/Film';
+
+class App extends React.Component {
+  render()  {
+    return (
+      <div>
+        <h1 className="appTitle">🍿Film Finder🍿</h1>
+        <SearchBar />
+      </div>
+    )
+  }
 }
 
 export default App
