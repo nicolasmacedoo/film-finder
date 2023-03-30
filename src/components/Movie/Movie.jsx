80496305c1feb5daea2d98d055316c5bf6164df8
Nicolas Macedo <nicolas.conceicao01@gmail.com>
2023-03-30 16:35:08 -0300

    :sparkles: feat: Pass the props to the fields

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -8,8 +8,17 @@ class Movie extends React.Component {
     render() {
         return (
             <div className="movieInfo">
-                <div className="moviePoster"></div>
-                <div className="movieText"></div>
+                <div className="moviePoster">
+                    <img 
+                        src={`https://image.tmdb.org/t/p/original/${this.props.movie.poster_path}`} 
+                        alt={`${this.props.movie.title} movie poster`} 
+                        id="moviePoster" 
+                    />
+                </div>
+                <div className="movieText">
+                    <h1 id="movieTitle">{this.props.movie.title}</h1>
+                    <p id="movieOverview">{this.props.movie.overview}</p>
+                </div>
             </div>
         )
     }
