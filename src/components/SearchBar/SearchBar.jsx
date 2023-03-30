8ef5d655727dee5fb7f8b696e68df23db28d698b
Nicolas Macedo <nicolas.conceicao01@gmail.com>
2023-03-30 15:22:03 -0300

    :sparkles: feat: show selected category only if is selected

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -4,13 +4,25 @@ import './SearchBar.css';
 class SearchBar extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            option: ''
+        };
+
+        this.handleOptionChange = this.handleOptionChange.bind(this)
+    }
+
+    handleOptionChange(event) {
+        this.setState({
+            option: event.target.value
+        });
     }
 
     render() {
         return (
             <div className="genre">
                 <p>Choose a genre</p>
-                <select name="genres" id="genres">
+                <select name="genres" id="genres" onChange={this.handleOptionChange}>
                     <option key="">Choose...</option>
                     {
                         this.props.genres.map(genre => {
@@ -19,7 +31,8 @@ class SearchBar extends React.Component {
                     }
                 </select>
                 <div>
-                    <button id="playBtn">Let's Play!</button>
+                    {this.state.option && <p>You selected {this.state.option}</p>}
+                    <button id="playBtn">Let's Play!</button>                   
                 </div>
             </div>
         )
